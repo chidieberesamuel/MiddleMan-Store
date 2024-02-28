ffe9cc4324e7dfdd56afc504a96e157d7c34cded
chidieberesamuel <chidieberesamuelalbert@gmail.com>
2024-02-28 12:37:03 +0100

    building a Math.min and percursion

--- index.js
+++ index.js
@@ -23,3 +23,53 @@ for (let row = 0; row < size; row++) {
   console.log(line);
 }
 
+
+
+function printfarmInventory(goat, meat) {
+  var goatString = String (goat);
+  while (goatString . length < 3)
+   goatString = "0" + goatString;
+  console.log(goatString + "goat");
+  var meatString = String (meat);
+  while (meatString . length < 3)
+   meatString = "0" + meatString;
+  console.log(meatString + "meat"); 
+}
+
+printfarmInventory(7, 11);
+
+
+function min(b, c) {
+  return Math.min(b, c);
+}
+
+console.log(min(4, 6));
+
+
+
+function isEven(number) {
+  if (number === 0) {
+    return true;
+
+  }
+  else if (number === 1) {
+    return false;
+  }
+  else if (number < 0) {
+    return isEven(-number);
+  }
+
+
+  if (number < 2) {
+    return isEven(number + 2);
+  }
+
+  return isEven(number - 2);
+  }
+
+
+  console.log(isEven(50));
+  console.log(isEven(75));
+  console.log(isEven(-1))
+
+ 
