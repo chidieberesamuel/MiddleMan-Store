d4a36b30b7ca4fac7f394390f2465a4d8c228564
Chidiebere <chidieberesamuelalbert@gmail.com>
2024-03-22 23:12:07 +0100

    The Document Object Model

--- index.js
+++ index.js
@@ -1,118 +1,60 @@
-function Vector(x, y) {
-  this.x = x;
-  this.y = y;
-}
-
-Vector.prototype.plus = function(vector) {
-  var newX = this.x + vector.x;
-  var newY = this.y + vector.y;
-  return new Vector(newX, newY);
-};
-
-Vector.prototype.minus = function(vector) {
-  var newX = this.x - vector.x;
-  var newY = this.y - vector.y;
-  return new Vector(newX, newY);
-};
-
-Object.defineProperty(Vector.prototype, 'length', {
-  get: function() {
-    return Math.sqrt(this.x * this.x + this.y * this.y);
-  }
-});
-
-var vector1 = new Vector(3, 4);
-var vector2 = new Vector(1, 2);
-
-var sumVector = vector1.plus(vector2);
-console.log(sumVector);
-
-var diffVector = vector1.minus(vector2);
-console.log(diffVector);
-
-console.log(vector1.length);
-console.log(vector2.length);
-
-
-function stretchCell(inner, width, height) {
-  this.inner = inner;
-  this.widt = width;
-  this.height = height;
-}
-
-stretchCell.prototype.minwidth = function() {
-  return Math.max(this.width, this.inner.minwidth());
-};
-
-stretchCell.prototype.minheight = function() {
-  return Math.max(this.height, this.inner.minheight());
-};
-
-stretchCell.prototype.draw = function(width, height) {
-  return this.inner.draw(width, this.height);
-};
-
-var innerCell = {
-  minwidth: function() {return 3; },
-  minheight: function() {return 3; },
-  draw: function(width, height) {
-    return Array(height).fill(Array(width).fill("X").join("")).j
-  }
-};
-
-var stretchCell = new stretchCell(innerCell,5, 3);
-console.log(stretchCell.minwidth());
-console.log(stretchCell.minheight());
-console.log(stretchCell.draw(5, 3));
-
-
-
-
-
-// function withBoxUnlock(){
-//   var mainBox = {
-//     unlocked: function () {
-//       this.unlock = true;
-//     },
-//     locked: function () {
-//       this.unlock = false;
-//     },
-//     unlock: true,
-//     getValue: [],
-//     display(){
-//       if (this.unlock) throw "UNLOCKED";
-//         return this.getValue;
-//     }
-//   }
-//   }
-
-
-//   console.log(unlock) 
-
-
-
-
-const testStrings = [
-  'The car is faster than expected  '
+const tableHeading = ["Name", "Height", "Country"];
+const tableValues = [
+  { Name: "Kilimajaro", Height: "5895",Country: "Tanzania"},
+  { Name: "Samuel", Height: "1.78",Country: "Nigeria"},
+  { Name: "Ronaldo", Height: "1.95",Country: "Portugal"},
 ];
 
-// Test each regular expression against the test strings
-const regVales = [
-  /ca[rt]/,
-  /pr?op/,
-  /fer(ret|ry|rari)/,
-  /\b\w+ious\b/,
-  /\s[.,:;]/,
-  /\b\w{7,}\b/,
-  /\b(?!.*e)\w+\b/,
-];
+function createTable(heading, values) {
+  var table = document.createElement("table");
+  var tableHead = document.createElement("thead");
+  var tableBody = document.createElement("tbody");
 
-regVales.forEach((regex, index) => {
-  console.log(`Regular expression ${index + 1}: ${regex}`);
-  testStrings.forEach((str) => {
-    const result = regex.test(str);
-    console.log(`"${str}" - Match found: ${result}`);
+  //Create table header row
+  var headerRow = document.createElement("tr");
+  heading.forEach(function(headingText) {
+     var th = document.createElement("th");
+     th.textContent = headingText;
+     headerRow.oppendchild(th);
+     // th,style.display = 'flex';
   });
-  console.log('---');
-});
+
+  tableHead.appendChild(headerRow);
+  table.appendChild(tableHead);
+  headerRow.style.fontSize = '40px';
+
+
+  //Create table body rows
+  values.forEach(function(rowData) {
+    var row = document.createElement("tr");
+    Object.values(rowData).forEach(function(value) {
+      var cell = document.createElementment("td");
+      cell.textContent = value;
+      row.appendChild(cell);
+    });
+    table.appendChild(tableBody);
+  });
+  table.appendChild(tableBody);
+
+  return table;
+}
+
+
+var tableContainer = document.getElementById("table-container");
+var table = createTable(tableHeading, tebleValues);
+tableContainer.appendChild(table);
+
+
+
+
+
+//Tagname 
+var paragraphs = document.getElementsByTagName("p");
+var paragraphValues = [];
+
+for (var i = 0; i < paragraphs.length; i++) {
+  paragraphValues.push(paragraphs[i].textContent);
+}
+console.log(paragraphValues);
+
 
